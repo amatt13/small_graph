2b826555c4bbb1b1bd8d2ccad2ad24c9d3d6a3d9
Anders Matthiassen <anders.matthiassen@gmail.com>
2018-03-03 07:32:49 +0100

    v3

--- pull data/traversed_path.py
+++ pull data/traversed_path.py
@@ -1,9 +1,7 @@
 import argparse
 import configparser
-import regex as re
-
+import pickle
 from itertools import groupby
-
 import psycopg2
 
 # Debug data
@@ -19,7 +17,6 @@ next_id = -1
 # All recorded segments (segment = edge-edge)
 segments = dict()
 
-
 def get_next_id():
     """
     ID producer
@@ -27,7 +24,7 @@ def get_next_id():
     """
     global next_id
     next_id += 1
-    return "#" + str(next_id)
+    return next_id
 
 
 def shorten(trip: tuple):
@@ -46,17 +43,115 @@ def shorten(trip: tuple):
         return trip[0], segments.get(trip[1])
 
 
-if __name__ == '__main__':
+# def hot_paths_old(trips):
+#     # Dict where the key is the concatenated seg_ids and the value is the trip_id and the number of times it have been
+#     # traversed
+#     traversals = dict()
+#     for trip in trips:
+#         traversals[trip[1]] = trip[0], 0
+#
+#     # Count and save the number of times that a path have been traversed
+#     traversals_length = len(traversals)
+#     count = 1
+#     print("Total count: " + str(traversals_length))
+#     for path in traversals:
+#         if count % 100 == 0:
+#             print(str(count) + "/" + str(traversals_length))  # progress update: mod 100
+#         elif count == traversals_length:
+#             print(str(count) + "/" + str(traversals_length))  # progress update: done(last)
+#         count += 1
+#         for trip in trips:
+#             occurrences = len(re.findall(path, trip[1], overlapped=True))
+#             if occurrences >= 1:
+#                 traversals[path] = traversals.get(path)[0], traversals.get(path)[1] + occurrences
+#
+#     results = []
+#     for path in traversals:
+#         value = traversals.get(path)
+#         if value[1] >= traverse_count:
+#             results.append((value[0], value[1]))
+#
+#     print("Done")
+#     print("Traversal Counts")
+#     for result in results:
+#         print(str(result[0]) + ":" + str(result[1]))
+
+
+frequent_paths = []
+
+
+def create_frequent_paths_1(trips: list, min_traversal: int):
+    # Count and save the number of times that a path have been traversed
+    frequent_paths.append(list())
+    edge_list = []
+    edge_counter = dict()
+    trips_length = len(trips)
+    count = 1
+    print("Total trips count: " + str(trips_length))
+    for trip in trips:
+        if count % 1000 == 0:
+            print(str(count) + "/" + str(trips_length))  # progress update: mod 100
+        elif count == trips_length:
+            print(str(count) + "/" + str(trips_length))  # progress update: done(last)
+        count += 1
+        for edge in trip[1]:
+            if edge in edge_list:
+                edge_counter[edge] = edge_counter.get(edge) + 1
+            else:
+                edge_list.append(edge)
+                edge_counter[edge] = 1
+    for edge in edge_counter:
+        if edge_counter[edge] >= min_traversal:
+            frequent_paths[0].append((edge,))
+
+
+def hot_paths(trips: list, x: int, cardinality: int):
+    trip_counter = {}
+    y = 2
+    while y <= cardinality:
+        print("Trips size: " + str(len(trips)))
+        print("frequent_paths_" + str(y-2) + " size: " + str(len(frequent_paths[y-2])))
+        print("Cardinality: " + str(y))
+        frequent_paths.append(list())
+        for trip in trips:
+            length = len(trip[1])
+            if length < y:
+                trips.remove(trip)
+            else:
+                i = 0
+                while i < length - y + 1:
+                    a = trip[1][i: i + y - 1]
+                    b = trip[1][i + 1: i + 1 + y - 1]
+                    if a in frequent_paths[y-2] and b in frequent_paths[y-2]:
+                        c = trip[1][i: i + 1 + y - 1]
+                        if trip_counter.get(c) is None:
+                            trip_counter[c] = 1
+                        else:
+                            trip_counter[c] = trip_counter[c] + 1
+                    i += 1
+        for path in trip_counter:
+            if trip_counter.get(path) > x:
+                frequent_paths[y-1].append(path)
+        trip_counter.clear()
+        y += 1
+
+    # Done, now go save everything
+    with open('create_frequent_paths', 'wb') as f:
+        pickle.dump(frequent_paths, f, pickle.HIGHEST_PROTOCOL)
+
+
+def db_stuff():
     parser = argparse.ArgumentParser(description='Download frequently traversed paths and report their properties')
     parser.add_argument('--ini', required=True,
                         help='The configuration file, e.g., transfer.ini')
-    parser.add_argument('-n',  type=str, required=False, help='Database name')
-    parser.add_argument('-i',  type=str, required=False, help='Database IP')
+    parser.add_argument('-n', type=str, required=False, help='Database name')
+    parser.add_argument('-i', type=str, required=False, help='Database IP')
     parser.add_argument('-po', type=str, required=False, help='Database port')
-    parser.add_argument('-u',  type=str, required=False, help='Username')
+    parser.add_argument('-u', type=str, required=False, help='Username')
     parser.add_argument('-pa', type=str, required=False, help='password')
-    parser.add_argument('-c',  type=str, required=False, help='frequent count')
+    parser.add_argument('-c', type=str, required=False, help='frequent count')
 
+    # argument handling / read ini file
     args = parser.parse_args()
     config = configparser.ConfigParser()
     config.read(args.ini)
@@ -94,59 +189,56 @@ if __name__ == '__main__':
     conn = psycopg2.connect(database="au_db", user=user, password=pw, host=ip, port=port)
     cur = conn.cursor()
 
-    cur.execute("SELECT trip_id, seg_id "
-                "FROM trips")
-    data = cur.fetchall()
-    #data = cur.fetchmany(10000)
+    print("Fetching...")
+    # cur.execute("SELECT m.trip_id, m.seg_id "
+    #             "FROM ( "
+    #             "   SELECT trip_id "
+    #             "   FROM trips "
+    #             "   WHERE trip_id >= 251627"
+    #             "   AND trip_id <= 3000000"
+    #             "   GROUP BY trip_id"
+    #             ") t "
+    #             "JOIN trips m "
+    #             "ON m.trip_id = t.trip_id")
 
-    # Sort by trip_id
-    sorted_data = sorted(data, key=lambda x: x[0])
-    del data
+    cur.execute("SELECT m.trip_id, m.seg_id "
+               "FROM ( "
+               "   SELECT trip_id "
+               "   FROM trips "
+               "   GROUP BY trip_id"
+               ") t "
+               "JOIN trips m "
+               "ON m.trip_id = t.trip_id")
+    data = cur.fetchall()
+    print("Data collected...\nMapping...")
 
     # map seg_ids to shorter names
-    sorted_data = [shorten(item) for item in sorted_data]
+    sorted_data = [shorten(item) for item in data]
 
     # Format sorted_data into a list that contains the trip_id and then all of the visited seg_ids
     # trip0, (seg1, seg2, seg3...)
     groups = []
     for k, g in groupby(sorted_data, lambda x: x[0]):
         groups.append(list(g))
-    del sorted_data
 
     # Concatenate the seg_id list into a single string
-    concatenated = []
+    trips = []
     for group in groups:
-        concatenated.append((group[0][0], ("".join([item[1] for item in group])) + "#"))
+        trips.append((group[0][0], tuple([item[1] for item in group])))
 
-    # Dict where the key is the concatenated seg_ids and the value is the trip_id and the number of times it have been
-    # traversed
-    traversals = dict()
-    for trip in concatenated:
-        traversals[trip[1]] = trip[0], 0
+    # Save everything
+    with open('all_data', 'wb') as f:
+        pickle.dump(trips, f, pickle.HIGHEST_PROTOCOL)
 
-    # Count and save the number of times that a path have been traversed
-    traversals_length = len(traversals)
-    count = 1
-    print("Total count: " + str(traversals_length))
-    for path in traversals:
-        if count % 100 == 0:
-            print(str(count) + "/" + str(traversals_length))  # progress update: mod 100
-        elif count == traversals_length:
-            print(str(count) + "/" + str(traversals_length))  # progress update: done(last)
-        count += 1
-        for trip in concatenated:
-            occurrences = len(re.findall(path, trip[1], overlapped=True))
-            if occurrences >= 1:
-                traversals[path] = traversals.get(path)[0], traversals.get(path)[1] + occurrences
+    return trips, traverse_count
 
-    results = []
-    for path in traversals:
-        value = traversals.get(path)
-        if value[1] >= traverse_count:
-            results.append((value[0], value[1]))
 
-    print("Done")
-    print("Traversal Counts")
-    for result in results:
-        print(str(result[0]) + ":" + str(result[1]))
+if __name__ == '__main__':
+    trips, traverse_count = db_stuff()
+    #with open('short', 'rb') as f:
+    #    X = pickle.load(f)
+    #sorted_data = pickle.load(open("data", "rb"))
+
+    create_frequent_paths_1(trips, traverse_count)
+    hot_paths(trips, traverse_count, 10)
     pass
