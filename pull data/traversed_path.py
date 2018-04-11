1c05c3b9c7b2c45fcd14a6b7f573418d856ef904
Anders Lykke Matthiassen <anders.matthiassen@gmail.com>
2018-04-11 21:36:21 +0200

    Documented and cleaned up traversed_path.py

--- pull data/traversed_path.py
+++ pull data/traversed_path.py
@@ -1,3 +1,9 @@
+"""
+    Created by Anders Lykke Matthiassen
+    amatt13@student.aau.dk OR
+    anders.matthiassen@gmail.com
+"""
+
 import argparse
 import configparser
 import pickle
@@ -5,21 +11,6 @@ from itertools import groupby
 import psycopg2
 
 FIRST_ENTRY_VALUE = "NA"
-
-# Debug data
-# (trip_id, seg_id)
-data = [('1', 'a-b'), ('1', 'b-c'), ('1', 'c-d'), ('1', 'c-d'), ('1', 'b-c'), ('1', 'c-d'),
-        ('2', 'b-c'), ('2', 'c-d'),
-        ('3', 'a-b'), ('3', 'a-b'), ('3', 'a-b'),
-        ('4', 'a-b'),
-        ('5', 'b-c'), ('5', 'c-d'), ('5', 'c-d')]
-
-gs = [[(1, '475808-452699'), (1, '411961-475808'), (1, '613448-411961'), (1, '53837-613448'), (1, '53837-475847'), (1, '67241-475847')],
-      [(2, '411261-508029'), (2, '393206-411261'), (2, '354873-393206'), (2, '258074-354873'), (2, '393205-258074'), (2, '482189-393205'), (2, '104475-482189'), (2, '385074-104475'), (2, '496282-385074'), (2, '102548-496282')],
-      [(3, '560042-627632'), (3, '32809-172893'), (3, '412495-32809'), (3, '247236-412495'), (3, '447808-247236'), (3, '627632-447808'), (3, '457610-560042'), (3, '308196-457610')],
-      [(4, '53837-475847'), (4, '67241-475847')],
-      [(5, '53837-475847'), (5, '67241-475847')],
-      [(6, '53837-475847'), (6, '67241-475847')]]
 
 # Short names for seg_id
 next_id = -1
@@ -56,86 +47,22 @@ def shorten(trip: tuple):
 frequent_paths = []
 
 
-def create_frequent_paths_1(trips: list, min_traversal: int):
-    # Count and save the number of times that a path have been traversed
-    frequent_paths.append(list())
-    edge_list = []
-    edge_counter = dict()
-    trips_length = len(trips)
-    count = 1
-    print("Total trips count: " + str(trips_length))
-    for trip in trips:
-        if count % 1000 == 0:
-            print(str(count) + "/" + str(trips_length))  # progress update: mod 100
-        elif count == trips_length:
-            print(str(count) + "/" + str(trips_length))  # progress update: done(last)
-        count += 1
-        for edge in trip[1]:
-            if edge in edge_list:
-                edge_counter[edge] = edge_counter.get(edge) + 1
-            else:
-                edge_list.append(edge)
-                edge_counter[edge] = 1
-    for edge in edge_counter:
-        if edge_counter[edge] >= min_traversal:
-            frequent_paths[0].append((edge,))
-
-
-def write_frequent(trip_counter: dict, x: int, y: int):
-    with open('frequent_paths_{0}'.format(str(y)), 'w') as f:
-        for path in trip_counter:
-            if trip_counter.get(path)[0] > x:
-                frequent_paths[y - 1].append(path)
-                f.write("{0};{1};{{{2}}};\n".format(
-                    ", ".join(path),
-                    str(trip_counter.get(path)[0]),
-                    ", ".join(str(trip_id) for trip_id in trip_counter.get(path)[1]))
-                )
-
-
-def hot_paths(trips: list, x: int, cardinality: int):
-    trip_counter = {}
-    y = 2
-    while y <= cardinality:
-        print("Trips size: " + str(len(trips)))
-        print("frequent_paths_" + str(y-2) + " size: " + str(len(frequent_paths[y-2])))
-        print("Cardinality: " + str(y))
-        frequent_paths.append(list())
-        for trip in trips:
-            length = len(trip[1])
-            if length < y:
-                trips.remove(trip)
-            else:
-                i = 0
-                while i < length - y + 1:
-                    a = trip[1][i: i + y - 1]
-                    b = trip[1][i + 1: i + 1 + y - 1]
-                    if a in frequent_paths[y-2] and b in frequent_paths[y-2]:
-                        c = trip[1][i: i + 1 + y - 1]
-                        if trip_counter.get(c) is None:
-                            trip_counter[c] = 1, [trip[0]]
-                        else:
-                            trip_counter[c] = trip_counter[c][0] + 1, trip_counter[c][1] + [trip[0]]
-                    i += 1
-        write_frequent(trip_counter, x, y)
-        trip_counter.clear()
-        y += 1
-
-    # Done, now go save everything
-    with open('create_frequent_paths', 'wb') as f:
-        pickle.dump(frequent_paths, f, pickle.HIGHEST_PROTOCOL)
-
-
-def fetch_data(save_to_file: bool, all_data: bool):
+def fetch_data(save_to_file: bool, all_data: bool = True):
+    """
+    Read input arguments and get data from DB
+    :param save_to_file: should the fetched data be saved in a file?
+    :param all_data: Use all of the data from the DB, or only a subset (debugging)
+    :return: All of the trips; The minimum traverse count; The max cardinality
+    """
     parser = argparse.ArgumentParser(description='Download frequently traversed paths and report their properties')
-    parser.add_argument('--ini', required=True,
-                        help='The configuration file, e.g., transfer.ini')
+    parser.add_argument('--ini', required=True, help='The configuration file, e.g., transfer.ini')
     parser.add_argument('-n', type=str, required=False, help='Database name')
     parser.add_argument('-i', type=str, required=False, help='Database IP')
     parser.add_argument('-po', type=str, required=False, help='Database port')
     parser.add_argument('-u', type=str, required=False, help='Username')
     parser.add_argument('-pa', type=str, required=False, help='password')
-    parser.add_argument('-c', type=str, required=False, help='frequent count')
+    parser.add_argument('-fc', type=str, required=False, help='frequent count')
+    parser.add_argument('-mc', type=str, required=False, help='max cardinality')
 
     # argument handling / read ini file
     args = parser.parse_args()
@@ -157,16 +84,21 @@ def fetch_data(save_to_file: bool, all_data: bool):
         pw = config.get('DEFAULT', 'password')
     else:
         pw = args.pa
-    if args.c is None:
+    if args.fc is None:
         traverse_count = config.get('DEFAULT', 'count')
     else:
-        traverse_count = args.c
+        traverse_count = args.fc
+    if args.mc is None:
+        cardinality = config.get('DEFAULT', 'cardinality')
+    else:
+        cardinality = args.mc
 
-    print("Frequently traversed count is: " + traverse_count)
+    print("Frequently traversed count is: {0}\nMax cardinality is: {1}".format(traverse_count, cardinality))
     traverse_count = int(traverse_count)  # change type to int...
+    cardinality = int(cardinality)
+
     conn = psycopg2.connect(database="au_db", user=user, password=pw, host=ip, port=port)
     cur = conn.cursor()
-
     print("Fetching...")
     if all_data:
         cur.execute("SELECT m.trip_id, m.seg_id "
@@ -182,7 +114,7 @@ def fetch_data(save_to_file: bool, all_data: bool):
         cur.execute("SELECT m.trip_id, m.seg_id "
                     "FROM ( "
                     "   SELECT trip_id "
-                    "   FROM trips "
+                    "   FROM trips_correction "
                     "   WHERE trip_id >= 295000"
                     "   AND trip_id <=   300000"
                     "   GROUP BY trip_id"
@@ -199,7 +131,9 @@ def fetch_data(save_to_file: bool, all_data: bool):
     for k, g in groupby(data, lambda x: x[0]):
         groups.append(list(g))
 
-    fixes = 0
+    # Fix mistakes in the DB
+    # Some trips have "teleporting" vehicles
+    # Whenever a vehicle teleports, a new trip is created
     for entry in groups:
         if len(entry) > 1:
             prev_dest = FIRST_ENTRY_VALUE
@@ -213,15 +147,10 @@ def fetch_data(save_to_file: bool, all_data: bool):
                     groups.remove(entry)
                     entry = entry[:-i]
                     i = 0
-                    fixes += 1
-                else:
-                    i = i
-                    pass
                 prev_dest = split[1]
                 i += 1
-    print(fixes)  # TODO remove me
 
-    # map seg_ids to shorter names
+    # map seg_ids to shorter names (easier debugging)
     # sorted_data = [shorten(item) for item in data]
 
     # Concatenate the seg_id list into a single string
@@ -235,52 +164,112 @@ def fetch_data(save_to_file: bool, all_data: bool):
         with open('all_data', 'wb') as f:
             pickle.dump(trips, f, pickle.HIGHEST_PROTOCOL)
 
-    return trips, traverse_count
+    return trips, traverse_count, cardinality
 
 
-if __name__ == '__main__':
-    # with open('create_frequent_paths', 'rb') as f:
-    #    X = pickle.load(f)
-    trips, traverse_count = fetch_data(save_to_file=True, all_data=True)
-    # with open('short', 'rb') as f:
-    #    X = pickle.load(f)
-    #trips = pickle.load(open("all_data", "rb"))
+def create_frequent_paths_1(trips: list, min_traversal: int):
+    """
+    Count and save the number of times that a path have been traversed
+    :param trips: a list of trips
+    :param min_traversal: minimum number of times that a trip has be traversed in order to be added to the result
+    :return: void (result is saved to the global list 'frequent_paths')
+    """
+    frequent_paths.append(list())
+    edge_list = []
+    edge_counter = dict()
+    trips_length = len(trips)
+    count = 1
 
-    create_frequent_paths_1(trips=trips, min_traversal=100)
-    hot_paths(trips=trips, x=100, cardinality=10)
+    # Count the number of times a trip have been traversed
+    print("Total trips count: " + str(trips_length))
+    for trip in trips:
+        if count % 1000 == 0:
+            print(str(count) + "/" + str(trips_length))  # progress update: mod 1000
+        elif count == trips_length:
+            print(str(count) + "/" + str(trips_length))  # progress update: done(last)
+        count += 1
+        for edge in trip[1]:
+            if edge in edge_list:
+                edge_counter[edge] = edge_counter.get(edge) + 1
+            else:
+                edge_list.append(edge)
+                edge_counter[edge] = 1
+
+    # Save those that meets the min_traversal variable requirement
+    for edge in edge_counter:
+        if edge_counter[edge] >= min_traversal:
+            frequent_paths[0].append((edge,))
+
+
+def hot_paths(trips: list, min_traversal: int, cardinality: int):
+    """
+    Find and save hot paths
+    :param trips: A list of trips
+    :param min_traversal: minimum number of times that a trip has be traversed in order to be added to the result
+    :param cardinality: The maximum number of edges to examine at the time (start at 2)
+    :return: void (results are saved in multiple files)
+    """
+    trip_counter = {}
+    y = 2  # Start cardinality
+    while y <= cardinality:
+        print("Trips size: " + str(len(trips)))
+        print("frequent_paths_" + str(y-2) + " size: " + str(len(frequent_paths[y-2])))
+        print("Cardinality: " + str(y))
+        frequent_paths.append(list())
+        for trip in trips:
+            length = len(trip[1])
+            if length < y:  # Only examine those trips which has at least y edges
+                # If it has less than y edges, it won't have y+1 edges either and should not be considered in the next iteration
+                trips.remove(trip)
+            else:
+                i = 0
+                while i < length - y + 1:
+                    a = trip[1][i: i + y - 1]
+                    b = trip[1][i + 1: i + 1 + y - 1]
+                    if a in frequent_paths[y-2] and b in frequent_paths[y-2]:
+                        c = trip[1][i: i + 1 + y - 1]
+                        if trip_counter.get(c) is None:
+                            trip_counter[c] = 1, [trip[0]]
+                        else:
+                            trip_counter[c] = trip_counter[c][0] + 1, trip_counter[c][1] + [trip[0]]
+                    i += 1
+        write_frequent(trip_counter, min_traversal, y)  # save trips in file
+        trip_counter.clear()
+        y += 1
+    # Done, now go save everything (debugging)
+    # with open('create_frequent_paths', 'wb') as f:
+    #     pickle.dump(frequent_paths, f, pickle.HIGHEST_PROTOCOL)
+
+
+def write_frequent(trip_counter: dict, min_traversal: int, cardinality: int):
+    """
+    Save trips in file and add them to the frequent_paths list
+    :param trip_counter: dict that contains the trips and the number of times they have been traversed
+    :param min_traversal: minimum number of times that a trip has be traversed
+    :param cardinality: current cardinality
+    :return: void (result is saved in a file 'frequent_paths_<cardinality>')
+    """
+    with open('frequent_paths_{0}'.format(str(cardinality)), 'w') as f:
+        for path in trip_counter:
+            if trip_counter.get(path)[0] > min_traversal:
+                frequent_paths[cardinality - 1].append(path)
+                f.write("{0};{1};{{{2}}};\n".format(  # read more about the format in the documentation document
+                    ", ".join(path),
+                    str(trip_counter.get(path)[0]),
+                    ", ".join(str(trip_id) for trip_id in trip_counter.get(path)[1]))
+                )
+
+
+def start():
+    """
+    This is just a function that can be called from other files if imported
+    Calling main is just as valid
+    """
+    trips, traverse_count, cardinality = fetch_data(save_to_file=True, all_data=True)
+    create_frequent_paths_1(trips=trips, min_traversal=traverse_count)
+    hot_paths(trips=trips, min_traversal=traverse_count, cardinality=cardinality)
     print("Done")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    start()
